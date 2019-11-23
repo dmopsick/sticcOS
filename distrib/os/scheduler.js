e41c95cb48fe709e30568c55c69764a7cb6d60b1
Dan Mopsick <dmop497@gmail.com>
2019-11-23 13:23:28 -0500

    Issue #48 abstracted out scheduling logic into switch to allow for implementation of priority and fcfs

--- distrib/os/scheduler.js
+++ distrib/os/scheduler.js
@@ -4,15 +4,37 @@ var TSOS;
     var Scheduler = /** @class */ (function () {
         // Initialize the scheduler
         function Scheduler() {
+            this.fcfsQuantum = Number.MAX_VALUE;
             this.quantum = _DefaultQuantum;
             this.readyQueue = new TSOS.Queue();
             this.cycleCounter = 0;
+            // Default the scheduling algorithm to RR
+            this.schedulingAlgorithm = 0;
         }
         // Issue #42 | Check the schedule on a CPU cycle and determine if a context switch is necesary
         Scheduler.prototype.checkSchedule = function () {
+            console.log(this.fcfsQuantum);
+            switch (this.schedulingAlgorithm) {
+                case 0: // Round robin
+                    this.roundRobinScheduling(this.quantum);
+                    break;
+                case 1: // FCSFS - Can be Round robin with high quantum (MaxInt)
+                    this.roundRobinScheduling(this.fcfsQuantum);
+                    break;
+                case 2:
+                    this.priorityScheduling();
+                    break;
+                default:
+                    // This should not be reached
+                    console.log("ERROR: Scheduling arlgorithm N/A");
+                    break;
+            }
             // Project 4... must check which scheduling algortihm is being used before just hopping into RR like I do right now
+        };
+        // Issue #48 | Abstracting out round robin functionality for use with round robin and FCFS
+        Scheduler.prototype.roundRobinScheduling = function (quantum) {
             // If the cycle counter is equal to the quantum, it is time for a context switch
-            if (this.cycleCounter >= this.quantum) {
+            if (this.cycleCounter >= quantum) {
                 // The counter has reached the quantum, time to load the next process
                 // Check if there are any waiting processes to switch to
                 if (this.readyQueue.getSize() > 0) {
@@ -32,6 +54,9 @@ var TSOS;
                 // Increment the cycle counter ... the current processes will execute, one cycle closer to next decision
                 this.cycleCounter++;
             }
+        };
+        Scheduler.prototype.priorityScheduling = function () {
+            console.log("Non preemptive priority scheduling");
         };
         return Scheduler;
     }());
